89def145c5061601348f93d95a4c1176db312a84
oclivar <oclivar2000@gmail.com>
2024-08-16 18:19:35 -0400

    Se aplicaron estilos con css y javascript a las cards creadas anteriormente

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,21 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+document.addEventListener("DOMContentLoaded", function() {
+    var buttons = document.querySelectorAll('.ver-mas');
+    
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var card = this.closest('.card'); // Encuentra la tarjeta padre más cercana
+
+            // Alterna entre las clases verde y azul
+            if (card.classList.contains('card-clicked-green')) {
+                card.classList.remove('card-clicked-green');
+                card.classList.add('card-clicked-blue');
+            } else if (card.classList.contains('card-clicked-blue')) {
+                card.classList.remove('card-clicked-blue');
+                card.classList.add('card-clicked-green');
+            } else {
+                // Si no tiene ninguna clase, asigna la clase verde por defecto
+                card.classList.add('card-clicked-green');
+            }
+        });
+    });
+});
