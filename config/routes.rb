1ea31911107f5d70f027d854434382355a2f9810
oclivar <oclivar2000@gmail.com>
2024-08-16 13:15:48 -0400

    Agregando 3 card a la aplicacion y alinerlos en forma horizontal

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/projects'
-  get 'pages/contact'
+
+   
+   root 'pages#home'
+   get '/home', to: 'pages#home'
+   get '/projects', to: 'pages#projects'
+   get '/contact', to: 'pages#contact'
+   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,5 +13,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # root "pages#home"
+
+
+
+
 end
